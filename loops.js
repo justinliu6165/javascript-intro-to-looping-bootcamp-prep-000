aadb45dae38d001ee5e50abc59cc9f58946ab829
Justin Liu <justin_liu@hotmail.co.uk>
2018-05-16 12:39:44 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,9 @@
+function forLoop(array){
+  array = [];
+  for(var i = 0; i<26; i++){
+      array.push(`I am ${i} strange loops.`);
+    // } else {
+    //   array.push(`I am ${i} strange loop.`);
+    }
+return array;
+}
