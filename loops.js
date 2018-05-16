f717c954b751a96a9e85d6c5ec20229ca3f026bd
Justin Liu <justin_liu@hotmail.co.uk>
2018-05-16 12:42:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,11 @@
 function forLoop(array){
   array = [];
   for(var i = 0; i<25; i++){
+    if(i !== 1){
       array.push(`I am ${i} strange loops.`);
     } else {
       array.push(`I am ${i} strange loop.`);
     }
+  }
 return array;
 }
